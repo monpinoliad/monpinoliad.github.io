705c340b26f5cd8a0eed8d638ce7eef4cac638e1
SPINOLIA <SPINOLIA@SPINOLIA-T480.ph.oracle.com>
2020-12-06 16:28:19 +0800

    Initial Commit

---
+++ precache-manifest.880dfcda71d728325e6d16ce23e0827a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adda89c0c1244b2d539a2e27da6dd813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4f77724668f49ef5622",
+    "url": "/static/css/main.3b75414d.chunk.css"
+  },
+  {
+    "revision": "805b926584646376adf0",
+    "url": "/static/js/2.ea3f0b82.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.ea3f0b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f77724668f49ef5622",
+    "url": "/static/js/main.3c860498.chunk.js"
+  },
+  {
+    "revision": "1460261ceb318b0edf1e",
+    "url": "/static/js/runtime-main.f3ba9008.js"
+  }
+]);
